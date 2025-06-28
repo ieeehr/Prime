e06d88906a017a1f1e82b5696f8bcc3f21979707
ieeehr <hridwick@ieee.org>
2025-06-28 18:35:20 +0530

    Added speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 //import Head from 'next/head'
 
 const geistSans = Geist({
@@ -38,6 +39,8 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
+
       </body>
     </html>
   );
